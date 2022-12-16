0cb428628c81904750bce269a4f6aa7a59287015
sara sallat <sallatsara@gmail.com>
2022-12-17 00:43:44 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Your code here
+class Polygon {
+  constructor(array){
+      this.array = array
+  }
+
+  get countSides(){
+      return this.array.length
+  }
+
+  get perimeter(){
+      let counter = 0
+      for(let i = 0; i < this.array.length; i++){
+          counter += this.array[i]
+      }
+      return counter
+  }
+}
+
+class Triangle extends Polygon{
+  constructor(...args){
+      super(...args)
+  }
+
+  get isValid(){
+      if((this.array[0] + this.array[1] > this.array[2]) && (this.array[1] + this.array[2] > this.array[0]) && (this.array[0] + this.array[2] > this.array[1])){
+          return true
+      }else{
+          return false
+      }
+  }
+}
+
+class Square extends Polygon{
+  constructor(...args){
+      super(...args)
+  }
+
+  get isValid(){
+      if(this.perimeter / 4 === this.array[0]){
+          return true
+      }else{
+          return false
+      }
+  }
+
+  get area(){
+      return this.array[0] * this.array[0]
+  }
+}
